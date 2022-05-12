b75d4b68f3e59ce16d182d3244832107c76ba110
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-12 02:22:56 +0000

    Update header file

--- monty.h
+++ monty.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -34,15 +35,50 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct line_s - Struct for manipulate each line in a file
+ * @number: Line number
+ * @tokens: Tokens of the line
+ *
+ * Description: Line in a file
+ */
+typedef struct line_s
+{
+	unsigned int number;
+	char **tokens;
+} line_t;
+
+/**
+ * struct global_s - file and stack
+ * @file: File descriptor
+ * @stack: stack_t list
+ *
+ * Description: file and stack
+ */
+typedef struct global_s
+{
+	FILE *file;
+	stack_t *stack;
+} global_t;
+
+/**
+ * struct arg_s - Argument values
+ * @n: Integer number
+ *
+ * Description: Argument values
+ */
+typedef struct arg_s
+{
+	int n;
+} arg_t;
+
+extern arg_t argument;
+
+
 /*Protoripe Functions */
+void (*get_op_func(line_t line, global_t *global))(stack_t **, unsigned int);
+void free_stack(stack_t **stack);
 
-
-
-
-
-
-
-
-
-
-#endif /*MAIN_H*/
+/* Checker Functions */
+void check_push(line_t line, global_t *global, char *opcode);
+#endif /*MONTY_H*/
